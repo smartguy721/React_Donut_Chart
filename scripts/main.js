c10f981f7d6253deef5adcdcd3c6e24e64337590
SmartGuy <smartguy@Andreis-Mac.local>
2016-05-10 04:53:23 +0300

    node server applied for json file getting by url

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,33 @@
-var ChartBox = React.createClass({
+var DonutChartBox = React.createClass({
+	loadChartDataFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({ data: data });
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadChartDataFromServer();
+	},
 	render: function() {
 		return (
-			<div className="chartBox">
-				Hello there!
+			<div className="dount-chart">
+				<ChartLabelList data={this.state.data} />
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<ChartBox />,
-	document.getElementById('donut-chart')
+	<DonutChartBox />,
+	document.getElementById('donut-chart-block')
 );
